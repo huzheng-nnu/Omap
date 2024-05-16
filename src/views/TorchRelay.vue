6d82758ccf8aa2f3ff044e8d5eb2f02aefa07b21
huzheng-nnu <256832672@qq.com>
2024-05-16 11:54:37 +0800

    TorchRelay

--- src/views/TorchRelay.vue
+++ src/views/TorchRelay.vue
@@ -1,47 +1,180 @@
 <template>
-	<!-- import TopMenu -->
-	<TopMenu />
+    <!-- import TopMenu -->
+    <div class="background"></div>
+  
+    <div class="front">
+      <TopMenu />
+  
+      <div class="midContainer">
+        <div class="sectionContainer">
+          <div class="sectionContainer2">
+            <!-- left -->
+            <div class="leftDiv">
+                <div class="imgContainer">
+
+                    <div class="contentDiv">
+                        <img src="../assets/touch_logo.svg" class="logoImg">
+                        <h2 class="titleText" lang="en">Olympic Torch Relay Stages</h2>
+                        <div class="contentText">
+                            <section class="contentSection">
+                                <p>A symbol of unity and peace, the Olympic torch spreads the spirit of the Games as it travels around the country, marking the start of celebrations. For the Paris 2024 Olympic Games, the Olympic Torch Relay will take place over a three-month period.</p>
+                            </section>
+                        </div>
+
+                        <!-- <div class="sc-ciOLHU kEqour">
+                            
+                        </div> -->
+                    </div>
 
 
-
-	<div class="background"></div>
-
-	<div class="front">
-	</div>
-
-
-
-</template>
-
-<script>
-import TopMenu from '@/components/TopMenu.vue'
-
-export default {
-  name: 'TorchRelay',
-  components: {
-    TopMenu
+                </div>
+            </div>
+  
+            <!-- right -->
+            <div class="rightDiv">
+              <MapboxMap />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import TopMenu from '@/components/TopMenu.vue'
+  import MapboxMap from '@/components/MapboxMap.vue';
+  
+  export default {
+    name: 'TorchRelay',
+    components: {
+      TopMenu,
+      MapboxMap
+    }
   }
-
-}
-</script>
-
-<style scoped>
-
-
-.background{
-    width:100%;  
-    height:100%;  /**宽高100%是为了图片铺满屏幕 */
-    z-index:-1;
+  </script>
+  
+  <style scoped>
+  .background {
+    width: 100%;
+    height: 100%;
+    z-index: -1;
     position: absolute;
-    /* background-image: url('../assets/background1.avif');
-    background-size: cover; 拉伸以填充容器 */
-} 
-
-.front{
-    z-index:1;
+  }
+  
+  .front {
+    z-index: 1;
     position: absolute;
     width: 100%;
     height: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  
+  .midContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80%;
+    height: 70%;
+    margin-top: 10%;
+  }
+  
+  .sectionContainer {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .sectionContainer2 {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: row;
+  }
+  
+  .leftDiv {
+    width: 30%;
+    visibility: visible;
+    /* background-image: url('../assets/background_topmenu.svg');
+    background-size: cover;  */
+  }
+  
+  .rightDiv {
+    width: 70%;
+    min-width: 70%;
+  }
+
+  .imgContainer{
+    height:100%;
+    width:100%;
+    background: linear-gradient(0deg, #FF5757 0%, #570047 100%);
+    /* background-size: cover;  */
+  }
+
+  .contentDiv{
+    /* color: rgb(0, 0, 0);
+    font-family: "Olympic Sans", Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -webkit-tap-highlight-color: transparent;
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+    --box-background: linear-gradient(0deg, #FF5757 0%, #570047 100%); */
+    padding: 2.4rem;
+  }
+
+  .titleText{
+  -webkit-font-smoothing: antialiased;
+    -webkit-tap-highlight-color: transparent;
+    border: 0px;
+    /* font-style: inherit;
+    font-variant: inherit;
+    font-stretch: inherit;
+    font-optical-sizing: inherit;
+    font-kerning: inherit;
+    font-feature-settings: inherit;
+    font-variation-settings: inherit;
+    margin: 0px; */
+    padding: 0px;
+    vertical-align: baseline;
+    font-family: "Paris 2024", "Olympic Headline", Helvetica, sans-serif;
+    font-size: 2.5rem;
+    font-weight: 900;
+    line-height: 2.5rem;
+    text-transform: uppercase;
+    letter-spacing: 0.05rem;
+    color: rgb(255, 255, 255);
+    display: inline-block;
+    -webkit-line-clamp: 5;
+    /* margin-top: 1rem; */
+    /* width: 40%; */
+    z-index: 1;
 }
 
-</style>
+.contentText{
+    /* font-size: 1.6rem;
+    line-height: 2.4rem; */
+    /* --box-background: linear-gradient(0deg, #FF5757 0%, #570047 100%); */
+    display: block;
+    width: 100%;
+    margin-top: 0rem;
+}
+.contentSection{
+    color: rgb(255, 255, 255);
+    font-family: "Olympic Sans", Arial, Helvetica, sans-serif;
+    font-size: 1.2rem;
+    line-height: 1.5rem;
+    --box-background: linear-gradient(0deg, #FF5757 0%, #570047 100%);
+    margin: 0px auto;
+    width: 100%;
+    overflow-wrap: break-word;
+}
+
+.logoImg {
+  position: relative;
+  width: auto;
+  height: 8rem;
+}
+  </style>
+  
